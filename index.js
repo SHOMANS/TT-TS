547c9f171f396f9c4d5eb1b78ab5dcd339a8e289
SHOMANS <mohammedshoman9@gmail.com>
2023-02-28 11:42:02 +0200

    finish typescript basics

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var message = 'Hello World!';
 console.log(message);
 message = '1';
@@ -26,4 +37,98 @@ var gender;
     gender[gender["male"] = 0] = "male";
     gender[gender["female"] = 1] = "female";
 })(gender || (gender = {}));
-var myGender = gender.male;
+var myGender = gender.female;
+var myGen = 'female';
+var today = 'mon';
+var something = '50';
+var numbe = 2;
+console.log(typeof something);
+var myNewFunc = function (number) {
+    console.log(typeof number);
+    return number;
+};
+myNewFunc(something);
+myNewFunc(numbe);
+var object3 = {
+    name: 'Anas',
+    age: 20
+};
+var object2 = {
+    name: 'Anas',
+    age: 20,
+    address: '',
+    func: function (name) {
+        return 1;
+    }
+};
+var object4 = {
+    name: 'Anas',
+    age: 20,
+    // address: '',
+    university: 'AUG'
+};
+// object4.name = 'anas2';
+object4.age = 25;
+var object5 = {
+    name: 'Anas',
+    age: 20,
+    address: '',
+    // title: '',
+    university: 'UP'
+};
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+var func1 = function (param) { return param; };
+var func2 = function (param) { return param; };
+var func3 = function (param) { return param; };
+var func4 = function (param) { return param; };
+// -----------------------------------------------------------------------------------------
+var genericFunction = function (param) { return param; };
+genericFunction('hello', '');
+genericFunction(6, false);
+genericFunction('sat', 5);
+genericFunction(gender.male, null);
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+var myArray = /** @class */ (function () {
+    function myArray(length, data) {
+        if (length === void 0) { length = 0; }
+        if (data === void 0) { data = {}; }
+        var _this = this;
+        this.length = length;
+        this.data = data;
+        this.getLength = function () {
+            return _this.length;
+        };
+        this.getItems = function () {
+            return _this.data;
+        };
+        this.push = function (item) {
+            var _a;
+            _this.data = __assign(__assign({}, _this.data), (_a = {}, _a[_this.length] = item, _a));
+            _this.length++;
+        };
+    }
+    return myArray;
+}());
+var arr2 = new myArray();
+console.log(arr2.getLength());
+console.log(arr2.getItems());
+arr2.push(1);
+arr2.push(1);
+arr2.push(1);
+console.log(arr2.getLength());
+console.log(arr2.getItems());
+console.log('*'.repeat(20));
+var arr3 = new myArray();
+console.log(arr3.getLength());
+console.log(arr3.getItems());
+arr3.push('first');
+arr3.push('second');
+arr3.push('third');
+arr3.push(1);
+arr3.push(false);
+console.log(arr3.getLength());
+console.log(arr3.getItems());
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
--- index.ts
+++ index.ts
@@ -67,4 +67,156 @@ enum gender {
   female,
 }
 
-const myGender: gender = gender.male;
+const myGender: gender = gender.female;
+
+type gender2 = 'male' | 'female' | 1 | 0;
+
+const myGen: gender2 = 'female';
+
+type week = 'sat' | 'sun' | 'mon' | 1;
+
+const today: week = 'mon';
+
+let something: string = '50';
+let numbe: number = 2;
+
+console.log(typeof something);
+
+const myNewFunc = (number: week): week => {
+  console.log(typeof number);
+  return number;
+};
+
+myNewFunc(something as week);
+myNewFunc(numbe as week);
+
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
+type myObject = {
+  readonly name: string;
+  age: number;
+  func?: (name: string) => void;
+  // func(name: string): void;
+};
+
+const object3: myObject = {
+  name: 'Anas',
+  age: 20,
+  // address: '',
+  // func(name: string) {},
+};
+
+// object3.name = 'askdjfh';
+
+type myObject2 = myObject & { address: string; func(name: string): number };
+
+const object2: myObject2 = {
+  name: 'Anas',
+  age: 20,
+  address: '',
+  func: (name: string) => {
+    return 1;
+  },
+};
+
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
+type uni = 'UP' | 'AUG' | 'IUG';
+
+interface myObject4 {
+  readonly name: string;
+  age: number;
+  university: uni;
+}
+
+const object4: myObject4 = {
+  name: 'Anas',
+  age: 20,
+  // address: '',
+  university: 'AUG',
+};
+
+// object4.name = 'anas2';
+object4.age = 25;
+
+interface myObject5 extends myObject4 {
+  address: string;
+  title?: string;
+}
+
+const object5: myObject5 = {
+  name: 'Anas',
+  age: 20,
+  address: '',
+  // title: '',
+  university: 'UP',
+};
+
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
+const func1: (param: number) => number = (param) => param;
+const func2: (param: string) => string = (param) => param;
+const func3: (param: boolean) => boolean = (param) => param;
+
+type general = string | number | boolean;
+
+const func4: (param: general) => general = (param) => param;
+
+// -----------------------------------------------------------------------------------------
+
+const genericFunction: <T, D>(param: T, param2: D) => T = (param) => param;
+
+genericFunction<string, string>('hello', '');
+genericFunction<number, boolean>(6, false);
+genericFunction<week, number>('sat', 5);
+genericFunction<gender, null>(gender.male, null);
+
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
+class myArray<T> {
+  constructor(private length: number = 0, private data: { [index: number]: T } = {}) {}
+
+  getLength = () => {
+    return this.length;
+  };
+
+  getItems = () => {
+    return this.data;
+  };
+
+  push = (item: T) => {
+    this.data = { ...this.data, [this.length]: item };
+    this.length++;
+  };
+}
+
+const arr2 = new myArray<number>();
+
+console.log(arr2.getLength());
+console.log(arr2.getItems());
+arr2.push(1);
+arr2.push(1);
+arr2.push(1);
+console.log(arr2.getLength());
+console.log(arr2.getItems());
+
+console.log('*'.repeat(20));
+
+const arr3 = new myArray<number | string | boolean>();
+
+console.log(arr3.getLength());
+console.log(arr3.getItems());
+arr3.push('first');
+arr3.push('second');
+arr3.push('third');
+arr3.push(1);
+arr3.push(false);
+console.log(arr3.getLength());
+console.log(arr3.getItems());
+
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
